c989875a6581f4d26e9e921b00f4ae0d064b7ee5
alkhair <alkhair.mohammd@gmail.com>
2024-05-04 18:17:54 +0200

    showing the ship and edit the screen height to make it shown

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 
 from settings import Settings
 
+from ship import Ship
+
 
 class AlienInvasion:
     """ Overall class to manage game assets and behavior """
@@ -14,6 +16,8 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
+
+        self.ship = Ship(self)
         """set the background color."""
 
     def run_game(self):
@@ -23,8 +27,11 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
