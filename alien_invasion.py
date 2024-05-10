7203daa62b99fcf91835eef23009c9cba31d357d
alkhair <alkhair.mohammd@gmail.com>
2024-05-10 14:19:11 +0200

    Examining and determining our code before working on aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,7 +12,7 @@ from bullet import Bullet
 class AlienInvasion:
     """ Overall class to manage game assets and behavior """
     def __init__(self):
-        """ Initialize the game, and create game resources"""
+        """ Initialize the game, and create game resources """
         pygame.init()
         self.settings = Settings()
 
@@ -23,16 +23,15 @@ class AlienInvasion:
         self.bullets = pygame.sprite.Group()
 
     def run_game(self):
-        """ start the main loop for the game """
+        """ Start the main loop for the game """
         while True:
-            # Watch for keyboard and mouse events.
+
             self._check_events()
 
             self.ship.update()
 
             self._bullets_update()
 
-            # Redraw the screen during each pass through the loop.
             self._update_screen()
 
     def _check_events(self):
